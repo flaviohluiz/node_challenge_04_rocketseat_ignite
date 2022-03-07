8bd3aefd1e944a51aff52f7cdbc724a01eae6842
Flavio Luiz <flavioluiz1@outlook.com>
2022-03-07 11:24:35 -0300

    first commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers;
+    try {
+      const allUserList = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.status(202).json(allUserList);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export { ListAllUsersController };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User doesn't exist!");
+    }
+
+    if (!user.admin) {
+      throw new Error("User provided is not an admin.");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
